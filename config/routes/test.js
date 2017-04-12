0abf8b3f5075c5f5b36cf55d28774c7585372d9a
Keith Irwin <mail@keithirwin.us>
2017-04-12 14:32:28 -0400

    Added formatting to push footer to bottom

--- config/routes/test.js
+++ config/routes/test.js
@@ -4,24 +4,30 @@ const router = require('express').Router(),
   mw = require('../middleware.js'),
   mail = require('../mail.js');
 
-router.route('/mail').get(function(req,res,next){
-	mail.send({
-	  to: `"Keith Irwin" <hypergeek14@gmail.com>`,
-	  from: mail.from,
-	  subject: 'Test email',
-	  text: mail.text("Looks like everything's working! "),
-	  html: mail.html("<p>Looks like everything's working! </p>")
-	}).then(function(){
-		console.log("Test email should have sent...");
-		res.sendStatus(200);
-	}).catch(function(err){
-	  mw.throwErr(err,req);
-	  next();
-	});
-});
+router
 
-router.route('/password').get(function(req,res){
-	res.render('password');
-});
+	.get('/mail', function(req,res,next){
+		mail.send({
+		  to: `"Keith Irwin" <hypergeek14@gmail.com>`,
+		  from: mail.from,
+		  subject: 'Test email',
+		  text: mail.text("Looks like everything's working! "),
+		  html: mail.html("<p>Looks like everything's working! </p>")
+		}).then(function(){
+			console.log("Test email should have sent...");
+			res.sendStatus(200);
+		}).catch(function(err){
+		  mw.throwErr(err,req);
+		  next();
+		});
+	})
+	
+	.get('/password', function(req,res){
+		res.render('password');
+	})
+	
+	.get('/short',function(req,res){
+		res.render('short');
+	});
 
 module.exports = router;
