bae3b10a1829a34db4ddcb0e3000c2ae1dd6fa99
Keith Irwin <mail@keithirwin.us>
2017-05-08 19:01:35 -0400

    #59 Finished contact form

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,10 +1,13 @@
 'use strict';
 
-const env = require('../env/env.js'),
+const mw = require('../middleware.js'),
+	env = require('../env/env.js'),
+	mail = require('../mail.js'),
 	router = require('express').Router(),
+	request = require('request'),
 	slug = require('slug'),
 	xss = require('xss'),
-  User = require('../models.js').user;
+	User = require('../models.js').user;
 
 module.exports = router
 	
@@ -21,11 +24,63 @@ module.exports = router
 	// Contact
 	.get('/contact', (req,res)=>{
 		res.render('contact',{
-			sitekey: env.recaptcha
+			sitekey: env.recaptchaSitekey
 		});
 	})
-	.post('/contact', (req,res)=>{
-		//TODO: Get contact stuff
+	.post('/contact', (req,res,next)=>{
+		
+		// Confirm captcha
+		request.post( 'https://www.google.com/recaptcha/api/siteverify', {form:{
+			secret: env.recaptchaSecret,
+			response: req.body['g-recaptcha-response'],
+			remoteip: req.ip
+		}}, (err, response, body)=>{
+			
+			// Check for errors
+			if (err){
+				mw.throwErr(err,req);
+				res.redirect('/contact');
+			}
+			if (response.statusCode!==200) {
+				let err = new Error('Bad response from reCaptcha service');
+				mw.throwErr(err,req);
+				res.redirect('/contact');
+			}
+			else {
+				
+				// Captcha succeeded
+				if (JSON.parse(body).success){
+					mail.send({
+						from: `${req.body.name} <${req.body.email}>`,
+						to: `Tracman Contact <contact@tracman.org>`,
+						subject: req.body.subject||'A message',
+						text: req.body.message
+					})
+					.then(()=>{
+						req.flash('success', `Your message has been sent. `);
+						res.redirect(req.session.next || '/');
+					})
+					.catch((err)=>{
+						mw.throwErr(err,req);
+						res.redirect('/contact');
+					});
+				}
+				
+				// Captcha failed
+				else {
+					let err = new Error('Failed reCaptcha');
+					mw.throwErr(err,req);
+					res.redirect('/contact');
+				}
+				
+			}
+		}
+);
+		
+		//TODO: Check req.body.g-recaptcha-response
+		
+		
+		
 	})
 	
 	// Terms of Service and Privacy Policy
