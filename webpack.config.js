a645eac478da9fd2b1293200c8e64b07f9c3bf7f
Keith Irwin <mail@keithirwin.us>
2018-03-05 05:54:34 +0000

    Deleted unneeded comment

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,6 @@ module.exports = {
     contact: './static/js/contact.js',
     login: './static/js/login.js',
     map: './static/js/map.js',
-    // controls: './static/js/controls.js',
     settings: './static/js/settings.js',
     password: './static/js/password.js',
     sw: './static/sw.js',
