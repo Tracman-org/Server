bc1a64c4fd507e26dcfa7f8d17069c74122a877d
Keith Irwin <mail@keithirwin.us>
2018-03-21 01:04:12 +0000

    Added id attr to submit button

--- static/js/settings/user.js
+++ static/js/settings/user.js
@@ -16,27 +16,20 @@ $(function () {
     }
   })
 
-  // Listen for change to name
-  $('#name-input').change(function () {
-    if ($('#name-input').val()) {
-      replaceFromEndpoint('xss', '#name-input')
-    }
-  })
-
   // Listen for change to email
   $('#email-input').change( function () {
 
     // Check email existence
     if (!$('#email-input').val()) {
       $('#email-help').show().text('You must enter an email address. ')
-      $('#submit-group .main')
+      $('#submit-btn')
         .prop('disabled', true)
         .prop('title', 'You need to enter an email address above. ')
 
-    // Check that email's different
+    // Check that email's changed
     } else if ($('#email-input').val() === original_email) {
       $('#email-help').hide()
-      $('#submit-group .main')
+      $('#submit-btn')
         .prop('disabled', false)
         .prop('title', 'Save your settings')
 
@@ -44,25 +37,23 @@ $(function () {
     } else if (!validateEmail($('#email-input').val())) {
       $('#email-help').show()
         .text('You must enter a valid email address. ')
-      $('#submit-group .main')
+      $('#submit-btn')
         .prop('disabled', true)
         .prop('title', 'You need to enter a valid email address above. ')
     } else {
 
       // Sanitize against mongo injection
-      // TODO: Do this client-side
       replaceFromEndpoint('mongo', '#email-input', function () {
 
         // Check server for uniqueness
-        $.ajax({
+        $.get({
           url: '/validate?email=' + $('#email-input').val(),
-          type: 'GET',
           statusCode: {
 
             // Is unique
             200: function () {
               $('#email-help').hide()
-              $('#submit-group .main')
+              $('#submit-btn')
                 .prop('disabled', false)
                 .prop('title', 'Save your settings')
             },
@@ -71,7 +62,7 @@ $(function () {
             400: function () {
               $('#email-help').show()
                 .text('That email is already in use by another account. ')
-              $('#submit-group .main')
+              $('#submit-btn')
                 .prop('disabled', true)
                 .prop('title', 'Supply a different email above')
             }
@@ -82,7 +73,7 @@ $(function () {
         }).fail(function () {
           $('#email-help').show()
             .text('Unable to confirm unique email.  Are you still connected to the internet?')
-          $('#submit-group .main')
+          $('#submit-btn')
             .prop('disabled', false)
             .prop('title', 'Try to save your settings')
         })
