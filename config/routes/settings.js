18a2c9ab2563c1407cf0e91e0793717bd1c9b657
Keith Irwin <mail@keithirwin.us>
2018-03-28 00:22:08 +0000

    #103 Added server-side changing of basic settings

--- config/routes/settings.js
+++ config/routes/settings.js
@@ -144,11 +144,11 @@ router.route('/maps')
   })
 
 // Get map settings
-router.get('/maps/:id', mw.ensureAuth, async (req, res, next) => {
+router.get('/maps/:map', mw.ensureAuth, async (req, res, next) => {
 
     // Find and populate associated map
     const found_map = await Map
-      .findById(sanitize(req.params.id))
+      .findById(sanitize(req.params.map))
       .populate({
         path: 'vehicles',
         populate: {
@@ -156,75 +156,78 @@ router.get('/maps/:id', mw.ensureAuth, async (req, res, next) => {
         },
       })
 
+    // Return map settings page
     if (!found_map) next() // 404
     else res.render('settings/map', {
       active: 'settings',
       map: found_map,
     })
+
   })
+// Redirect faulty POSTs
+router.get('/maps/:map/basics', (req, res) => {
+  res.redirect(`/settings/maps/${req.params.map}#basics`)
+})
+router.get('/maps/:map/display', (req, res) => {
+  res.redirect(`/settings/maps/${req.params.map}#display`)
+})
+router.get('/maps/:map/vehicles', (req, res) => {
+  res.redirect(`/settings/maps/${req.params.map}#vehicles`)
+})
+router.get('/maps/:map/admins', (req, res) => {
+  res.redirect(`/settings/maps/${req.params.map}#admins`)
+})
 
 // Set basic map settings
-router.post('/maps/:id/basic', async (req, res, next) => {
-
-  // Validate slug
-  const checkSlug = new Promise( async (resolve, reject) => {
-    // Check existence
-    if (req.body.slug === '') {
-      req.flash('warning', `You must supply a slug.  `)
-      resolve()
-
-    // Check if unchanged
-    } else if (req.user.maps[0].slug === slug(xss(req.body.slug))) resolve()
-
-    // Check uniqueness
-    else {
-      try {
-        let existingUser = await User.findOne({ slug: req.body.slug })
-
-        // Not unique!
-        if (existingUser && existingUser.id !== req.user.id) {
-          req.flash( 'warning',
-          `That slug, <u>${req.body.slug}</u>, is already in use by another map! `
-          )
-
-        // It's unique
-        } else req.user.maps[0].slug = slug(xss(req.body.slug))
-
-        resolve()
-      } catch (err) { reject() }
-    }
-  })
-
-  // Set settings when done
+router.post('/maps/:map/basics', async (req, res, next) => {
   try {
-    await checkSlug
-    debug('Setting map settings... ')
 
-    // Set values
-    req.user.maps[0].settings = {
-      // TODO: Rescheme this
-      units: req.body.units,
-      defaultMap: {
-        type: req.body.defaultMapType,
-        zoom: req.body.defaultZoom,
-        lat: req.body.defaultLat,
-        lon: req.body.defaultLon,
-      },
-      showScale: !!(req.body.showScale),
-      showSpeed: !!(req.body.showSpeed),
-      showAlt: !!(req.body.showAlt),
-      showStreetview: !!(req.body.showStreet),
-      marker: req.body.marker
-    }
+    // Get map
+    const map = await Map.findById(sanitize(req.params.map))
+    if (!map) throw new Error(`Could not find map with ID ${req.params.map}!`)
+
+    // Validate slug
+    const checked_slug = new Promise( async (resolve, reject) => {
+      debug(`Checking slug ${req.body.slug}... `)
+      try {
+
+        // Check existence
+        if (req.body.slug === '')
+          throw new Error('You must supply a slug.  ')
+  
+        // Check uniqueness
+        else {
+          const valid_slug = slug(xss(req.body.slug))
+          const existing_map = await Map.findOne({ slug: valid_slug })
+  
+          // Not unique!
+          if (existing_map && existing_map.id !== req.params.map)
+            throw new Error('That slug is already in use by another map! ')
+  
+          // It's unique
+          else resolve(valid_slug)
+        }
+        
+      } catch (err) { reject(err) }
+      
+    })
+
+    // Set settings
+    debug(`Setting map name to ${xss(req.body.name)} and slug to ${await checked_slug}... `)
+    map.slug = await checked_slug
+    map.name = xss(req.body.name)
 
     // Save user and send response
-    debug(`Saving new settings for map ${req.user.maps[0].name}...`)
-    await req.user.save()
+    debug(`Saving new settings for map ${map.name}...`)
+    await map.save()
+
+    // Success
     debug(`DONE!  Redirecting...`)
     req.flash('success', 'Settings updated. ')
 
   } catch (err) { mw.throwErr(err, req) }
-  finally { res.redirect('/settings/maps') }
+  finally { res.redirect(`/settings/maps/${req.params.map}#basics`) }
+
 })
 
 // Set display settings
