1aacc7284c2e776abd3d66336acb8622ba511683
Keith Irwin <mail@keithirwin.us>
2018-03-28 18:35:26 -0400

    Fixed CSP by specifying https for google analytics

--- static/js/base.js
+++ static/js/base.js
@@ -7,7 +7,7 @@
     (t[m].q = t[m].q || []).push(arguments)
   }, t[m].l = 1 * new Date(); o = r.createElement(a),
 n = r.getElementsByTagName(a)[0]; o.async = 1; o.src = c; n.parentNode.insertBefore(o, n)
-})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga')
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
 ga('create', 'UA-44266909-3', 'auto')
 ga('require', 'linkid')
 ga('send', 'pageview')
